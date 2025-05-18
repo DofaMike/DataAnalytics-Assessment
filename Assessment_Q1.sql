4180fd9702585eaaf348e874b2fe5619d5a4ab91
Udofa Mfon-Abasi Michael <150776110+DofaMike@users.noreply.github.com>
2025-05-18 20:49:34 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,7 +1,7 @@
 SELECT 
     b.owner_id, 
     CONCAT(a.first_name, ' ', a.last_name) AS name,  
-    COUNT(DISTINCT b.id) AS savings_count, 
+    COUNT(DISTINCT b.savings_id) AS savings_count, 
     COUNT(DISTINCT c.id) AS investment_count, 
     SUM(b.confirmed_amount) AS total_deposits
 FROM users_customuser a
